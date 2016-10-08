3ebfc1dff0c06da6493312024a8b54306cce2be7
Carson Schmidt <carsonschmidt@Carsons-MacBook-Pro.local>
2016-10-08 17:21:02 -0400

    parking spot class changed

---
+++ ParkGreen/app/src/main/java/mhacks8/parkgreen/ParkingSpotHandler.java
@@ -0,0 +1,10 @@
+package mhacks8.parkgreen;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by carsonschmidt on 10/8/16.
+ */
+
+public class ParkingSpotHandler extends SQLiteOpenHelper {
+}
