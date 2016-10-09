d1f03707a3febb18df5afefebe3f3bc644105104
Carson Schmidt <carsonschmidt@Carsons-MacBook-Pro.local>
2016-10-08 22:55:51 -0400

    passanger activity in progress

--- ParkGreen/app/src/main/java/mhacks8/parkgreen/Passanger1.java
+++ ParkGreen/app/src/main/java/mhacks8/parkgreen/Passanger1.java
@@ -2,12 +2,18 @@ package mhacks8.parkgreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Passanger1 extends AppCompatActivity {
+
+    EditText passanger_code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passanger1);
+
+        passanger_code = (EditText) findViewById(R.id.passanger_code);
+
     }
 }
